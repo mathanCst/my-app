546b4beb0ccf12f42dd6d46b745342e1a23d397f
Mathan Thangavadivel <MZT@cisi.org>
2023-01-03 23:28:37 +0530

    int commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
+//import { render } from "react-dom"
+import { createStore, applyMiddleware, Store} from "redux"
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from "react-redux"
+import thunk from "redux-thunk"
 
+import reducer from "./store/reducer"
+
+const store: Store<ArticleState, ArticleAction> & {
+  dispatch: DispatchType
+} = createStore(reducer, applyMiddleware(thunk))
+
+// const reducer = combineReducers({
+//   counter: counter.reducer,
+//   user: user.reducer,
+// })
+// const store1 = configureStore({
+//   reducer,
+//   middleware: applyMiddleware(thunk),)
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <Provider store={store}>
+  <App />
+</Provider>,
 );
 
 // If you want to start measuring performance in your app, pass a function
